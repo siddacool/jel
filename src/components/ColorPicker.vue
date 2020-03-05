b0cf1a30177ea3d9e4c747f3fadfe1d31303779f
Siddhesh Mangela <sidthecool007@gmail.com>
2020-03-05 15:10:45 +0530

    ColorPicker component separated

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <chrome-picker
+    :value="backdrop"
+    @input="updateValue"
+    class="color-picker-ui"
+  />
+</template>
+
+<script>
+import { Chrome } from 'vue-color';
+
+export default {
+  name: 'ColorPicker',
+  data() {
+    return {
+      backdrop: this.$store.state.backdrop
+    };
+  },
+  components: {
+    'chrome-picker': Chrome
+  },
+  methods: {
+    updateValue(e) {
+      this.$store.dispatch('changeBackDrop', e);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.color-picker-ui {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+}
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,43 +1,24 @@
 <template>
   <div class="home-landing">
     <Surface />
-    <chrome-picker
-      :value="backdrop"
-      @input="updateValue"
-      class="color-picker"
-    />
+    <ColorPicker />
   </div>
 </template>
 
 <script>
-import { Chrome } from 'vue-color';
 import Surface from '../components/Surface.vue';
+import ColorPicker from '../components/ColorPicker.vue';
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      backdrop: this.$store.state.backdrop
-    };
-  },
   components: {
     Surface,
-    'chrome-picker': Chrome
-  },
-  methods: {
-    updateValue(e) {
-      this.$store.dispatch('changeBackDrop', e);
-    }
+    ColorPicker
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.color-picker {
-  position: fixed;
-  z-index: 1000;
-  top: 0;
-  left: 0;
-}
+
 </style>
