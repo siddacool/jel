41461d743ce9242e12311d6a2c446ad4f9eea17f
Siddhesh Mangela <sidthecool007@gmail.com>
2020-03-13 15:23:29 +0530

    swatches added to context menu

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -1,25 +1,54 @@
 <template>
-  <vue-context>
+  <vue-context :close-on-click="false" v-show="!isColorPickerVisible">
     <li>
       <a href="#" @click.prevent="toggleColorPicker">
         Color Picker
         <span class="shortcut">alt + p</span>
       </a>
     </li>
+    <li>
+      <div class="group">
+        <p>Swatches</p>
+        <div class="context_swatches">
+          <li
+            v-for="(swatch, index) in swatches"
+            :key="index"
+            :class="swatch.selected ? 'selected' : null"
+            @click.prevent="activeSwatch(index)"
+          >
+            <backdrop :color="swatch.color" />
+          </li>
+        </div>
+      </div>
+    </li>
   </vue-context>
 </template>
 
 <script>
 import { VueContext } from 'vue-context';
+import backdrop from './backdrop.vue';
 
 export default {
   name: 'ContextMenu',
   components: {
-    'vue-context': VueContext
+    'vue-context': VueContext,
+    backdrop
+  },
+  computed: {
+    swatches() {
+      return this.$store.state.swatches;
+    },
+    isColorPickerVisible() {
+      // `this` points to the vm instance
+      return this.$store.state.isColorPickerVisible;
+    }
   },
   methods: {
     toggleColorPicker() {
       this.$store.dispatch('toggleColorPicker');
+    },
+    activeSwatch(index) {
+      this.$store.dispatch('activateSwatch', index);
     }
   }
 };
@@ -40,7 +69,7 @@ export default {
 .v-context ul {
   font-size: 13px;
   min-width: 14rem;
-  min-height: 10rem;
+  padding: 0;
 }
 
 .v-context > li > a,
@@ -62,7 +91,47 @@ export default {
   top: 2px;
 }
 
+.v-context > li > .group,
+.v-context ul > li > .group {
+  padding: 0.9rem 1rem;
+  position: relative;
+}
+
+.v-context > li > .group > p,
+.v-context ul > li > .group > p {
+  font-size: 13px;
+  font-weight: 500;
+  text-align: left;
+  margin-bottom: 10px;
+  color: #212529;
+}
+
 .shortcut {
   font-weight: 600;
+}
+
+.context_swatches {
+  display: flex;
+  margin: 0;
+  justify-content: space-between;
+}
+
+.context_swatches li {
+  display: block;
+  width: 23px;
+  height: 23px;
+  border: 1px solid #ececec;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.context_swatches li.selected {
+  box-shadow: 0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12);
+  border: none;
+  transform: scale(1.1, 1.1);
+}
+
+.context_swatches li div {
+  border-radius: inherit;
 }
 </style>
