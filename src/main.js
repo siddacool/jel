715e54ac623532a40f7bed5978baa3b687bb3dd3
Siddhesh <sidthecool007@gmail.com>
2020-03-17 14:10:48 +0530

    modal plugin init

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import './variables.css';
 import store from './store';
 import backdrop from './plugins/backdrop';
 import iconbtn from './plugins/icon-btn';
+import modal from './plugins/modal';
 
 Vue.config.productionTip = false;
 Vue.use(backdrop);
 Vue.use(iconbtn);
+Vue.use(modal);
 
 new Vue({
   store,
