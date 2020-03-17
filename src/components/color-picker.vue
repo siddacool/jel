ca53d8229596b3de7c60aa4bc361599e589ce552
Siddhesh <sidthecool007@gmail.com>
2020-03-17 11:57:02 +0530

    Dark theme quick switch removed

--- src/components/color-picker.vue
+++ src/components/color-picker.vue
@@ -5,16 +5,6 @@
     v-show="show"
     :style="position ? `top:${position.y}px; left:${position.x}px` : null"
   >
-   <icon-btn
-      :action="toggleDarkTheme"
-      :darkTheme="darkTheme"
-      class="color-picker_dark-mode-toggle"
-      svgSize="large"
-      size="medium"
-      noGlass
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.088 13.126l1.912-1.126-1.912-1.126c-1.021-.602-1.372-1.91-.788-2.942l1.093-1.932-2.22-.02c-1.185-.01-2.143-.968-2.153-2.153l-.02-2.219-1.932 1.093c-1.031.583-2.34.233-2.941-.788l-1.127-1.913-1.127 1.913c-.602 1.021-1.91 1.372-2.941.788l-1.932-1.093-.02 2.219c-.01 1.185-.968 2.143-2.153 2.153l-2.22.02 1.093 1.932c.584 1.032.233 2.34-.788 2.942l-1.912 1.126 1.912 1.126c1.021.602 1.372 1.91.788 2.942l-1.093 1.932 2.22.02c1.185.01 2.143.968 2.153 2.153l.02 2.219 1.932-1.093c1.031-.583 2.34-.233 2.941.788l1.127 1.913 1.127-1.913c.602-1.021 1.91-1.372 2.941-.788l1.932 1.093.02-2.219c.011-1.185.969-2.143 2.153-2.153l2.22-.02-1.093-1.932c-.584-1.031-.234-2.34.788-2.942zm-10.117 6.874c-4.411 0-8-3.589-8-8s3.588-8 8-8 8 3.589 8 8-3.589 8-8 8zm.029-2c-3.313 0-6-2.687-6-6s2.687-6 6-6v12z"/></svg>
-    </icon-btn>
     <icon-btn
       :action="hideColorPicker"
       :darkTheme="darkTheme"
@@ -107,9 +97,6 @@ export default {
   methods: {
     activeSwatch(index) {
       this.$store.dispatch('activateSwatch', index);
-    },
-    toggleDarkTheme() {
-      this.$store.dispatch('toggleDarkTheme');
     }
   }
 };
@@ -233,11 +220,11 @@ export default {
   border-radius: inherit;
 }
 
-.color-picker_dark-mode-toggle {
+/* .color-picker_dark-mode-toggle {
   position: absolute;
   top: 10px;
   left: calc(50% - 20px);
-}
+} */
 
 /* dark theme */
 
