299be53d0f10993c0d6c142b1e0434d39fddda6f
Siddhesh <sidthecool007@gmail.com>
2020-03-18 16:42:18 +0530

    deploy 🔥

---
+++ precache-manifest.8a67dc5df01605a6515baf945606982d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a964cdbc7d42bc959e",
+    "url": "/jel/css/about.9bf7649e.css"
+  },
+  {
+    "revision": "cd3987f7cef5981fab0e",
+    "url": "/jel/css/app.3689f95d.css"
+  },
+  {
+    "revision": "d9c714ee57164cd07169",
+    "url": "/jel/css/chunk-0752f4da.929f4ee4.css"
+  },
+  {
+    "revision": "ba0d024d8942a9586305",
+    "url": "/jel/css/chunk-7bd89ebb.83cf0baf.css"
+  },
+  {
+    "revision": "915b4b36d8deead68350",
+    "url": "/jel/css/context-menu.939bed07.css"
+  },
+  {
+    "revision": "29007f024a91ae7e9a6e",
+    "url": "/jel/css/mobile-menu.be685d42.css"
+  },
+  {
+    "revision": "ac2d190f753c92275b3582eea03c1e33",
+    "url": "/jel/favicon.svg"
+  },
+  {
+    "revision": "6189344757f7d17e37467e43d8f3ab7a",
+    "url": "/jel/index.html"
+  },
+  {
+    "revision": "33a964cdbc7d42bc959e",
+    "url": "/jel/js/about.91f20e59.js"
+  },
+  {
+    "revision": "cd3987f7cef5981fab0e",
+    "url": "/jel/js/app.683cbc16.js"
+  },
+  {
+    "revision": "d9c714ee57164cd07169",
+    "url": "/jel/js/chunk-0752f4da.0eaaf4a4.js"
+  },
+  {
+    "revision": "ba0d024d8942a9586305",
+    "url": "/jel/js/chunk-7bd89ebb.4c951969.js"
+  },
+  {
+    "revision": "4cede7d76284e1a8fdc3",
+    "url": "/jel/js/chunk-vendors.596dd27c.js"
+  },
+  {
+    "revision": "915b4b36d8deead68350",
+    "url": "/jel/js/context-menu.e3c80bb9.js"
+  },
+  {
+    "revision": "29007f024a91ae7e9a6e",
+    "url": "/jel/js/mobile-menu.448dfd12.js"
+  },
+  {
+    "revision": "fa8b5d1d83dc173264e231e878d32239",
+    "url": "/jel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jel/robots.txt"
+  }
+]);
